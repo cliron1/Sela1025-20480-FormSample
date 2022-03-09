d816f663b911daa4f6b639fc674f0506e1b2a093
liron <liron@flame-ware.com>
2022-03-09 15:55:34 +0200

    first commit

---
+++ FormsSample/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using FormsSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FormsSample.Controllers {
+	public class HomeController: Controller {
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger) {
+			_logger = logger;
+		}
+
+		public IActionResult Index() {
+			return View();
+		}
+
+		public IActionResult Privacy() {
+			return View();
+		}
+
+		public IActionResult Contact() {
+			var model = new ContactModel();
+			return View(model);
+		}
+
+		public IActionResult ContactSubmit(ContactModel model) {
+
+			_logger.LogInformation("{name}, {mobile}, {email}", model.FullName, model.Mobile, model.Email);
+
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error() {
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ FormsSample/Models/ContactModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormsSample.Models {
+	public class ContactModel {
+		[Required]
+		public string FullName { get; set; }
+		public string Email { get; set; }
+		public string Mobile { get; set; }
+		public string Subject { get; set; }
+		public string Body { get; set; }
+	}
+}
